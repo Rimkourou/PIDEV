d7c101517e721230a0fab93e6d1c8b0cb38579f4
wifek <wifek.fouzai@esprit.tn>
2021-03-07 10:19:20 +0100

    API Rating

--- src/InterfaceFilm/InterfaceDetailFilmController.java
+++ src/InterfaceFilm/InterfaceDetailFilmController.java
@@ -1,13 +1,29 @@
 package InterfaceFilm;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import org.controlsfx.control.Rating;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.Label;
 
 public class InterfaceDetailFilmController implements Initializable {
+    @FXML
+    private Rating rating;
+
+    @FXML
+    private Label test;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        rating.ratingProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number old, Number t1) {
+                test.setText("Rating: " +t1);
+            }
+        });
     }
 }
