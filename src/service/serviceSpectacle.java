966bf5d5a2bd14b79a5de31270867cd0b120c4a0
sanasaied <78903231+sanasaied@users.noreply.github.com>
2021-03-25 21:06:29 +0100

    Update serviceSpectacle.java

--- src/service/serviceSpectacle.java
+++ src/service/serviceSpectacle.java
@@ -1,19 +1,27 @@
 package service;
 
 import Entités.SpectacleE;
+import javafx.collections.ObservableList;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 
 public interface serviceSpectacle {
 
     void AddSpectacle(SpectacleE s);
 
-    List<SpectacleE> spectacleEList();
+    //List<SpectacleE> spectacleEList();
 
     void deleteSpectacle(int id) throws SQLException;
 
     void updateSpectacle(SpectacleE s);
 
 
+    ObservableList<SpectacleE> rechercheSpectacle(String critereRech, Date dteDeb, Date dteFin);
+
+    ObservableList<SpectacleE> rechercheSpectacle(String critereRech, String valRech) throws ParseException;
+
+    boolean spectacleExist(String titre) throws SQLException;
 }
