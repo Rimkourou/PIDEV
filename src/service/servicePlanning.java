b455578641726563e223d737b3f499d3b19a0b53
sanasaied <78903231+sanasaied@users.noreply.github.com>
2021-03-25 21:11:48 +0100

    Update servicePlanning.java

--- src/service/servicePlanning.java
+++ src/service/servicePlanning.java
@@ -2,17 +2,33 @@ package service;
 
 
 import Entités.Planning;
+import javafx.collections.ObservableList;
 
-import java.util.List;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
 
 public interface servicePlanning {
-    void AddPlanning(Planning p);
-    List<Planning> planningList();
-    void deletePlanning(Planning p);
+
+    //Suppression de planning
+    void deletePlanning(int id);
+        //Mise à jour
     void updatePlanning(Planning p);
 
+    //Selectionner planings from db
+    ObservableList<Planning> LoadPlanningFromDatabase();
+
+    //Selectionner planings from db
+    String loadImageEventFromDB(Integer idE, String typeE);
+
+    ArrayList<Integer> loadIdEventSalleFromDB(String tableName);
+
+    void AddPlanning(Planning p);
+
+    ObservableList<Planning> recherchePlanning(String filtreRech, String text);
+
+    ObservableList<Planning> recherchePlanning(String selectedItem, Date dateDeb, Date dateFin);
 
 
-
+    String selectRoleCurrentUser(Integer id);
 }
-
