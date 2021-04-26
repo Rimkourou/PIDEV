26d472c245eb6d5ebb53539a86fb6d2c1a6d5849
wifek <wifek.fouzai@esprit.tn>
2021-04-27 00:08:56 +0100

    intégration rim+feriel

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,7 @@ return [
     CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Ob\HighchartsBundle\ObHighchartsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
